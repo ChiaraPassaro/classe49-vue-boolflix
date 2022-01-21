78848c85b3945efd3a1fe548e81cc1cf6c38e499
Chiara Passaro <chiarapassaro.wd@gmail.com>
2022-01-21 09:56:10 +0100

    add images flag

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,57 @@
 <template>
   <div class="card">
-    {{ info.title }}
-    {{ info.origina_title }}
-    {{ info.original_language }}
-    {{ info.language }}
-    {{ info.vote_average }}
+    <h2>
+      {{ info.title }}
+    </h2>
+    <h3>
+      {{ info.original_title }}
+    </h3>
+    <div class="language">
+      <!-- <img src="../assets/images/en.png" alt=""> -->
+      <img
+        v-if="isAvailable"
+        :src="require(`../assets/images/${info.original_language}.png`)"
+        :alt="info.original_title"
+      >
+      <p v-else>
+        {{ info.original_language }}
+      </p>
+    </div>
+    <div class="vote">
+      {{ info.vote_average }}
+    </div>
   </div>
 </template>
 
 <script>
+/**
+ * TODO
+ * Trasformiamo la stringa statica della lingua in una vera
+ * e propria bandiera della nazione corrispondente,
+ * gestendo il caso in cui non abbiamo la bandiera della nazione
+ * ritornata dall’API (le flag non ci sono in FontAwesome).
+ * "original_language": "de"
+ */
 export default {
   name: 'Card',
   props: {
     info: Object,
+  },
+  data() {
+    return {
+      availableLanguage: [
+        'en',
+        'it',
+      ],
+    };
+  },
+  computed: {
+    isAvailable() {
+      if (this.availableLanguage.includes(this.info.original_language)) {
+        return true;
+      }
+      return false;
+    },
   },
 };
 </script>
